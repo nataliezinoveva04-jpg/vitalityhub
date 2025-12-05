ad92835991833e80c628785b3ec6da129c97426e
nataliezinoveva04-jpg <nataliezinoveva04@gmail.com>
2025-12-05 20:16:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def submit_feedback():
     return jsonify({"status": "success", "message": f"Thank you, {name}, for your feedback!"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
